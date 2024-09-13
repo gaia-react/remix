d36319365cc020e9abdff7dd76551d607deebf91
Steven Sacks <stevensacks@gmail.com>
2024-09-14 00:10:33 +0900

    ci: add npm_package_version to env

--- .storybook/vite.config.ts
+++ .storybook/vite.config.ts
@@ -2,8 +2,7 @@ import {defineConfig, loadEnv} from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  process.env = {...process.env, ...env};
+  Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
 
   return {
     plugins: [tsconfigPaths()],
