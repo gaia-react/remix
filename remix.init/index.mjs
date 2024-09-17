a99e3d1111842c823a725c4c724aef948bd26ffb
Steven Sacks <stevensacks@gmail.com>
2024-09-17 15:37:54 +0900

    fix: remix.init path

--- remix.init/index.mjs
+++ remix.init/index.mjs
@@ -3,8 +3,8 @@ import {rename} from 'node:fs';
 import path from 'node:path';
 
 const initializeEnvironment = () => {
-  const originalFile = path.resolve('./.env.example');
-  const renamedFile = path.resolve('./.env');
+  const originalFile = path.resolve('.env.example');
+  const renamedFile = path.resolve('.env');
   rename(originalFile, renamedFile, (error) => {
     if (error) {
       console.error('ERROR:', error);
