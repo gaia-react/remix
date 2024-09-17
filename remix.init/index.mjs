fb915ee057c7a05934ed6adde43ebbe3439be90f
Steven Sacks <stevensacks@gmail.com>
2024-09-17 15:43:12 +0900

    fix: remix.init path

--- remix.init/index.mjs
+++ remix.init/index.mjs
@@ -1,15 +1,10 @@
-/* eslint-disable no-console */
-import {rename} from 'node:fs';
+import {copyFileSync} from 'node:fs';
 import path from 'node:path';
 
 const initializeEnvironment = () => {
   const originalFile = path.resolve('.env.example');
   const renamedFile = path.resolve('.env');
-  rename(originalFile, renamedFile, (error) => {
-    if (error) {
-      console.error('ERROR:', error);
-    }
-  });
+  copyFileSync(originalFile, renamedFile);
 };
 
 const main = () => {
